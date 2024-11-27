dbb4487518d346999d9f58bc589c99ce331f429d
Favour Ebrusike <ebrusikefavour@gmail.com>
2024-11-27 20:31:24 +0100

    first push

---
+++ product-api/data/products.go
@@ -0,0 +1,68 @@
+package data
+
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+type Product struct {
+	ID        int `json:"id"`
+	Name      string `json:"name"`
+	Description string `json:"description"`
+	Price     float32  `json:"price"`
+	SKU       string `json:"sku"`
+	CreatedOn string `json:"-"`
+	UpdatedOn string `json:"-"`
+	DeletedOn string `json:"-"`
+}
+
+
+type Products []*Product
+
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+
+func (p *Products) FromJSON(r io.Reader) error{
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+}
+
+
+func GetProducts() Products{
+	return productList
+}
+
+func AddProduct(p *Product){
+   p.ID = getNextID()
+   productList = append(productList, p)
+}
+
+func getNextID() int{
+	lp := productList[len(productList) -1]
+	return lp.ID + 1
+}
+
+var productList = []*Product{
+	&Product{
+		ID:        1,
+		Name:      "Lattle",
+		Description:      "Frothy milky coffee",
+		Price:     2.45,
+		SKU:       "abc123",
+		CreatedOn: time.Now().UTC().String(),
+		UpdatedOn: time.Now().UTC().String(),
+	},
+	&Product{
+		ID:        2,
+		Name:      "Esspresso",
+		Description:      "Short and strong coffee without milk",
+		Price:     1.99,
+		SKU:       "abc123",
+		CreatedOn: time.Now().UTC().String(),
+		UpdatedOn: time.Now().UTC().String(),
+	},
+}
